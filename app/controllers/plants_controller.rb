e44a8586e9110be9c980942c2d25b52a2b590c5f
George Chaves <george_hfc@yahoo.com.br>
2022-07-19 19:45:13 +0100

    generated plants controller

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,2 @@
+class PlantsController < ApplicationController
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "user@mail.com", encrypted_password: "123")
+
+puts "Cleaning database..."
+Plant.destroy_all
+
+puts "Creating plants..."
+plantome = { name: "Plantome",
+             type: "",
+             description: "Lorem ipsum",
+             image_url: "https://images.unsplash.com/photo-1600411833196-7c1f6b1a8b90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80",
+             care_level: 0,
+             location: "Barcelona, Spain",
+             user_id: 1 }
+planthouse = { name: "Plant House",
+               type: "Rose",
+               description: "Awesome plant",
+               image_url: "https://images.unsplash.com/photo-1584589167171-541ce45f1eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+               care_level: 2,
+               location: "Lisbon, Portugal",
+               user_id: 1 }
+
+[ plantome, planthouse ].each do |attributes|
+  plant = Plant.create!(attributes)
+  puts "Created #{plant.name}"
+end
+puts "Finished!"
