43e2b2b20ea14dc6e1384e6df27b3017e8a03ab2
Hope Fletcher <thehopefletcher@gmail.com>
2022-07-19 21:20:32 +0200

    Updated devise sign-up forms with first_name, last_name, user_name and role

---
+++ db/migrate/20220719183300_add_sign_up_requirementsto_users.rb
@@ -0,0 +1,8 @@
+class AddSignUpRequirementstoUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :role, :string
+    add_column :users, :username, :string
+  end
+end
