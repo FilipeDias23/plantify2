94d3656562bb352e1a173caa5e7b1b1c56d86e61
Hope Fletcher <thehopefletcher@gmail.com>
2022-07-20 12:54:05 +0200

    added the 4 crud actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,29 @@
 class BookingsController < ApplicationController
   #TODO index, show, new, create
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @Booking = Booking.find(params[:id])
+  end
+
+  def new
+    @plant = Plant.find(params[:plant_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @plant = Plant.find(params[:plant_id])
+
+    @booking.plant = @plant
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
 end
