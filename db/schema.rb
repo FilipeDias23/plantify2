de99ba59c72370ab0f2fce340f4f455fb2fabbc9
Claudia Troeger <claudiatroeger2410@yahoo.de>
2022-07-21 19:22:14 +0100

    Added price to plants and image_pic to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_164144) do
+ActiveRecord::Schema.define(version: 2022_07_21_182037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_07_20_164144) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
@@ -49,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_07_20_164144) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.string "profile_img"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
