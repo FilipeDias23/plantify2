5a4a6e4fce6091d8359e4636048e2ab7accc2ed3
George Chaves <george_hfc@yahoo.com.br>
2022-07-28 23:08:41 +0100

    care level selector

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,6 +2,7 @@ class Plant < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates_presence_of :name, :plant_type, :care_level, :location, :description, :price
+  validates :care_level, length: { in: 0..3 }
   has_one_attached :image, dependent: :destroy
 
   # Geocoder
